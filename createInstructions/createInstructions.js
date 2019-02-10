93bff2433cebe7891f2e2e1d1b4ec2740f7245bc
Pablo <pablo@intvs.org>
2019-02-10 21:52:01 +0000

    Create the createInstructions.js

---
+++ createInstructions/createInstructions.js
@@ -0,0 +1,4 @@
+// This function will recieve an array of objects with the grid and
+// the coordinates of every house in need of pizza delivery.
+// The function will return a string with the instructions for the pizza robot.
+
