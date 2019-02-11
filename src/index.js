3ecb79a4b2146febea2c8dfda2d04d74bbef24db
Pablo <pablo@intvs.org>
2019-02-11 18:44:25 +0000

    Clean Up index.js

--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ const formater = require('./formater/formater');
 // Import the plotter function.
 const plotter = require('./plotter/plotter');
 
-//  Get the string input
+//  Get the input string
 const inputString = process.argv[2];
 
-// Create a map with the grid and the locations requested.
+/* Create a mapper with the grid and the coordinates of the houses requested. 
+* Finally, return a list of instructions for the pizzabot.
+*/
 const getMapper = formater(inputString);    
 const getInstructions = plotter(getMapper);
 console.log(getInstructions);
