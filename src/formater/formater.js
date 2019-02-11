b575f614fb13311b1baba81328bb27f71da804b5
Pablo <pablo@intvs.org>
2019-02-11 18:44:19 +0000

    Clean up formater

--- src/formater/formater.js
+++ src/formater/formater.js
@@ -1,4 +1,5 @@
-/* This function will receive as an input the string with the grid a list * * of points representing houses in need of pizza delivery. 
+/* This function will receive as an input string with the grid and 
+* a list of points representing houses in need of pizza delivery. 
 * The function will return a array of objects with the grid and the coordinates of each house.
 */
 
@@ -10,20 +11,22 @@ function formater (inputString){
   };
 
   const search = (query) => {
+    // Convert the grid string to an Object.
     const grid = {
       x: parseInt(query.split(' ')[0].split('x')[0], 10),
       y: parseInt(query.split(' ')[0].split('x')[1], 10),
-    }    
+    }  
 
+    // Convert the coords of the houses to an Array of Objects.
     const coords = query.match(/\(.*?\)/g).map(coord=>{
       const arr = coord.replace(/[{( )}]/g, '').split(',')
       return {x: parseInt(arr[0], 10), y: parseInt(arr[1], 10)}
     })
-    
+      
     let result = {
-      grid: grid,
-      coords: coords
-    }
+      grid,
+      coords
+    }    
 
     return result;
   }
