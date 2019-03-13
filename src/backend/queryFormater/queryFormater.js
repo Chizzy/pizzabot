994e5ab30a71393ef45a1f8f5a60fda9131e3d83
Pablo <pablo@intvs.org>
2019-03-13 10:42:55 +0000

    Add Function helper for QueryString

---
+++ src/backend/queryFormater/queryFormater.js
@@ -0,0 +1,21 @@
+/* This function will receive as an input string with the grid and 
+* a list of points representing houses in need of pizza delivery. 
+* The function will return a array of objects with the grid and the coordinates of each house.
+*/
+
+function queryFormater (gridX, gridY, coordX, coordY, coordX_2, coordY_2){
+  const objQuery = {}
+  switch (true) {
+    case (!coordX || !coordY):
+    objQuery.string = gridX + 'x' + gridY + ' (' + coordX_2 + ', ' + coordY_2 + ')'
+    break;
+    case (!coordX_2 || !coordY_2):
+    objQuery.string =  gridX + 'x' + gridY + ' (' + coordX + ', ' + coordY + ')'
+    break;
+    default:
+    objQuery.string = gridX + 'x' + gridY + ' (' + coordX + ', ' + coordY + ')' + ' (' + coordX_2 + ', ' + coordY_2 + ')'
+  }
+  return objQuery.string;
+
+}
+module.exports = queryFormater
